860e9ce93ee7571f859a6b7a9626e797267569e4
srisaiomkar <srisaiomkar@gmail.com>
2021-10-23 19:02:46 -0400

    removed unnecesary comments

--- publisher/fetchDataFromAPI.js
+++ publisher/fetchDataFromAPI.js
@@ -1,27 +1,3 @@
-// const request = require('request');
-// async function fetchDataFromAPI(Topic) {
-//     const options = {
-//         method: 'GET',
-//         url: 'https://amazon-products1.p.rapidapi.com/search',
-//         qs: { country: 'US', query: Topic, page: '1' },
-//         headers: {
-//             'x-rapidapi-host': 'amazon-products1.p.rapidapi.com',
-//             'x-rapidapi-key': '52dbbe839dmshe6180372ad28496p1d28fcjsnc17ef4914a33',
-//             useQueryString: true
-//         }
-//     };
-
-//     const response = request(options);
-//     console.log("response",response)
-//     const data = {
-//         "topicName": Topic,
-//         "results": response.results
-//     }
-
-//     return data;
-
-// }
-
 var axios = require("axios").default;
 
 module.exports = function fetchDataFromAPI(Topic){
--- publisher/publishTopics.js
+++ publisher/publishTopics.js
@@ -1,7 +1,3 @@
-//const TopicIphone12 = require('./TopicIphone12');
-//const TopicMacBookPro = require('./TopicMacBookPro');
-//const TopicMysteryBooks = require('./TopicMysteryBooks');
-
 const express = require('express');
 const request = require('request');
 const fetchDataFromAPI = require('./fetchDataFromAPI');
@@ -16,23 +12,22 @@ const TopicMacBook = "MACBOOK";
 const TopicMysteryBooks = "MYSTERY BOOKS";
 //Topics 1,2,3
 
-// publish(fetchDataFromAPI(TopicIphone))
-// publish(fetchDataFromAPI(TopicMacBook))
-// fetchDataFromAPI(TopicIphone).then(data => {
-//     var model ={
-//             "topicName": TopicIphone,
-//             "results": data
-//         }
-//     publish(model)
-// });
 
-// fetchDataFromAPI(TopicMysteryBooks).then(data => {
-//     var model ={
-//             "topicName": TopicMysteryBooks,
-//             "results": data
-//         }
-//     publish(model)
-// });
+fetchDataFromAPI(TopicIphone).then(data => {
+    var model ={
+            "topicName": TopicIphone,
+            "results": data
+        }
+    publish(model)
+});
+
+fetchDataFromAPI(TopicMysteryBooks).then(data => {
+    var model ={
+            "topicName": TopicMysteryBooks,
+            "results": data
+        }
+    publish(model)
+});
 
 fetchDataFromAPI(TopicMacBook).then(data => {
     var model ={
@@ -41,15 +36,6 @@ fetchDataFromAPI(TopicMacBook).then(data => {
         }
     publish(model)
 });
-// fetchDataFromAPI(TopicMacBook).then(data => publish(data));
-// fetchDataFromAPI(TopicMysteryBooks).then(data => publish(data));
-//const MysteryandSuspenseBooks = fetchDataFromAPI('Mystery and suspense books ');
-//const MacBook = fetchDataFromAPI('MacBook');
-
-
-//publish(MysteryandSuspenseBooks)
-
-
 
 function publish(data) {
 
